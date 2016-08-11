08b9c2e45fb26159f2fc060f37b52a93ab29527d
IltonS <ilton.sequeira.s@gmail.com>
2016-08-11 19:06:52 -0300

    Add Hardware acceleration CSS

--- platforms/android/assets/www/controllers/FindAPartyController.js
+++ platforms/android/assets/www/controllers/FindAPartyController.js
@@ -1,5 +1,5 @@
 angular.module('FindAParty')
-  .controller('FindAPartyController', function($scope,$mdSidenav){
+  .controller('FindAPartyController', function($scope, $timeout, $mdSidenav, $log){
     $scope.toggleLeft = function(){
       $mdSidenav('left').toggle();
     };
@@ -16,4 +16,56 @@ angular.module('FindAParty')
       }
       return input;
     };
+    
+    /*    
+    $scope.toggleLeft = buildToggler('left');
+    $scope.toggleRight = buildToggler('right');
+    $scope.isOpenRight = function(){
+      return $mdSidenav('right').isOpen();
+    };
+    
+    function debounce(func, wait, context) {
+      var timer;
+      return function debounced() {
+        var context = $scope,
+            args = Array.prototype.slice.call(arguments);
+        $timeout.cancel(timer);
+        timer = $timeout(function() {
+          timer = undefined;
+          func.apply(context, args);
+        }, wait || 300);
+      };
+    }
+    
+    function buildDelayedToggler(navID) {
+      return debounce(function() {
+        // Component lookup should always be available since we are not using `ng-if`
+        $mdSidenav(navID)
+          .toggle()
+          .then(function () {
+            $log.debug("toggle " + navID + " is done");
+          });
+      }, 300);
+    }
+    
+    function buildToggler(navID) {
+      return function() {
+        // Component lookup should always be available since we are not using `ng-if`
+        $mdSidenav(navID)
+          .toggle()
+          .then(function () {
+            $log.debug("toggle " + navID + " is done");
+          });
+      };
+    }
+    
+    $scope.closeLeft = function () {
+      // Component lookup should always be available since we are not using `ng-if`
+      $mdSidenav('left').close()
+        .then(function () {
+          $log.debug("close LEFT is done");
+        });
+    };
+    */
+    
   });
