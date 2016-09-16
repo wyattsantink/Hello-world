cd4fa24e0c5bfa5109657faa95dd005537f5c70a
IltonS <ilton.sequeira.s@gmail.com>
2016-09-16 10:35:49 -0300

    Add Parties -> show

--- www/controllers/PartiesController.js
+++ www/controllers/PartiesController.js
@@ -22,8 +22,15 @@ angular.module('FindAParty')
       this.party = Party.findById($routeParams.id);
       var that = this;
       this.party.$loaded().then(function(data){
+        //Enable the edit button:
         that.partyIsEditable = that.party.endsAt.timestamp > Date.now();
+        //set the page's selected address:
         that.searchAddress();
+        //If in /Parties/show, also loads the hoster information:
+        if($location.path().substring(0,13) === '/Parties/show'){
+          //Get Party's hoster info:
+          that.hoster = User.findById(that.party.hoster);
+        }
       }); 
     }
     
